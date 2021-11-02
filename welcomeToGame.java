f607d51845e855c0f049ec17f0c6e89df6b7bd5f
Emelie Strömberg <90707506+emeliestromberg@users.noreply.github.com>
2021-11-02 13:31:37 +0100

    Add files via upload

---
+++ welcomeToGame.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class welcomeToGame {
+    public static void main(String[] args) {
+        System.out.println("\nHello! Welcome to a game of rock paper scissor!\nIt's so good to have you here!");
+
+    }
+
+}
